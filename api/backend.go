a67ef9e602ff34b0957232479797fb5d80037277
maggie-5miles <maggie@wespoke.com>
2018-05-29 11:52:15 +0800

    remove call to TxPool.SetTMClient

--- api/backend.go
+++ api/backend.go
@@ -117,9 +117,6 @@ func (b *Backend) Config() *eth.Config {
 func (b *Backend) SetTMNode(tmNode *tmn.Node) {
 	b.chainID = tmNode.GenesisDoc().ChainID
 	b.localClient = rpcClient.NewLocal(tmNode)
-	// uncomment this for TxPool broadcast tx to tendermint directly,
-	// the TxPool must has SetTMClient method when uncomment this
-	b.ethereum.TxPool().SetTMClient(b.localClient)
 }
 
 func (b *Backend) PeerCount() int {
