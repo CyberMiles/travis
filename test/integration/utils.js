e3a34036ac4b0077bc0689fbef72d70e38f51fcf
maggie-5miles <maggie@wespoke.com>
2018-08-09 15:06:32 +0800

    test: fix bug, web3.cubeSign was removed.

--- test/integration/utils.js
+++ test/integration/utils.js
@@ -429,10 +429,41 @@ const delegatorAccept = (delegator, validator, deleAmount) => {
     validatorAddress: validator,
     amount: deleAmount,
     cubeBatch: Globals.CubeBatch,
-    sig: web3.cubeSign(delegator, nonce)
+    sig: cubeSign(delegator, nonce)
   }
   let r = web3.cmt.stake.delegator.accept(payload)
   expectTxSuccess(r)
+}
+
+const crypto = require("crypto")
+// this private key is for testing only, use it together with cubeBatch "01"
+const privateKey = `-----BEGIN RSA PRIVATE KEY-----
+MIICXQIBAAKBgQCiWpvDnwYFTqgSWPlA3VO8u+Yv9r8QGlRaYZFszUZEXUQxquGl
+FexMSVyFeqYjIokfPOEHHx2voqWgi3FKKlp6dkxwApP3T22y7Epqvtr+EfNybRta
+15snccZy47dY4UcmYxbGWFTaL66tz22pCAbjFrxY3IxaPPIjDX+FiXdJWwIDAQAB
+AoGAOc63XYz20Nbz4yyI+36S/UWOLY/W8f3eARxycmIY3eizilfE5koLDBKm/ePw
+2dvHJTdBDI8Yu9vWy3Y7DWRNOHJKdcc1gGCR36cJFc4/h02zdaK+CK4eAaZLXhdK
+H8DljEx6QAeRtxVLZGeYa4actY+3GeujYvkQ5QwNprchTSECQQDO4VMmLB+iIT/N
+jnADYOuWKe3iLBoTKHmVfAaTRMMeHATMkpgyVzTLO7jMYCWy7+S0DL4wDNUTQv+P
+Nna/hrAxAkEAyObfMAgjnW6s+CGoN+yWtdBC0LvDXDrzaT3KqmHxK2iCg2kQ9R6P
+0vCvGJytuPxmIVZn54+KpKfR6ok6RJSbSwJAF+CRxDobfI7x2juyWfF5v18fgZct
+e0CUp9gkuiKZkoQRWbshrc263ioKbiw6rahacR13ZfxVK1/0NwdGNVzKQQJBAJpw
+QGpgF2DSz8z/sp0rFsA1lOd5L7ka6Dui8MUB/a9s68exYQPNtqpls3SsHS/zd19x
+WPa9dcsV51zwmQZXZvkCQQChnQLBs6BbH6O85ePXSSbe7RUvHua6EEkmCNkIw+vT
+3Jqmk4ecxCzmEv3xbzrCdgOhfjxqjrsqLLK6BH+lJsWS
+-----END RSA PRIVATE KEY-----`
+
+const cubeSign = (address, nonce) => {
+  let message = address + "|" + nonce
+
+  let signer = crypto.createSign("sha256")
+  signer.write(message)
+  signer.end()
+
+  let signature = signer.sign(privateKey)
+  let signature_hex = signature.toString("hex")
+  logger.debug("cube sig: ", signature_hex)
+  return signature_hex
 }
 
 module.exports = {
@@ -455,5 +486,6 @@ module.exports = {
   removeFakeValidators,
   calcAwards,
   calcDeleAwards,
-  delegatorAccept
+  delegatorAccept,
+  cubeSign
 }
