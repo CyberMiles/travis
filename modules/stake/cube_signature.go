3b71b1df3b2a9b53c85a1f5ba041ff63e89319b4
dragon <longzhi@gmail.com>
2018-08-13 14:17:53 +0800

    Verify the signatures of delegation transactions to make sure they come from the CMT Cube.

--- modules/stake/cube_signature.go
+++ modules/stake/cube_signature.go
@@ -1,18 +1,23 @@
 package stake
 
 import (
-	"fmt"
-	"strings"
-	"crypto/sha256"
+	"bytes"
 	"crypto/rsa"
-	"crypto"
-	"encoding/pem"
+	"crypto/sha256"
 	"crypto/x509"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/CyberMiles/travis/utils"
+	"encoding/hex"
 	"encoding/json"
+	"encoding/pem"
+	"fmt"
 	travis "github.com/CyberMiles/travis/types"
+	"github.com/CyberMiles/travis/utils"
+	"github.com/ethereum/go-ethereum/common"
+	"math/big"
+	"strings"
 )
+
+var bigZero = big.NewInt(0)
+var bigOne = big.NewInt(1)
 
 func VerifyCubeSignature(address common.Address, nonce uint64, cubeBatch string, sig string) error {
 	message := fmt.Sprintf("%s|%d", strings.ToLower(address.String()), nonce)
@@ -30,10 +35,18 @@ func VerifyCubeSignature(address common.Address, nonce uint64, cubeBatch string,
 		return err
 	}
 
-	err = rsa.VerifyPKCS1v15(pk, crypto.SHA256, hashed[:], common.Hex2Bytes(sig))
+	m := new(big.Int)
+	bs, err := hex.DecodeString(sig)
 	if err != nil {
-		fmt.Printf("Error occurred while verifying the signature: %s\n", err)
+		fmt.Printf("decode signature error: %v\n", err)
 		return err
+	}
+
+	m.SetBytes(bs)
+	c := encrypt(new(big.Int), pk, m)
+
+	if !bytes.Equal(c.Bytes(), hashed[:]) {
+		return ErrInvalidCubeSignature()
 	}
 
 	return nil
@@ -54,6 +67,39 @@ func getCubePublicKeyString(cubeBatch string) (string, error) {
 	}
 
 	return "", nil
+}
+
+func modInverse(a, n *big.Int) (ia *big.Int, ok bool) {
+	g := new(big.Int)
+	x := new(big.Int)
+	g.GCD(x, nil, a, n)
+	if g.Cmp(bigOne) != 0 {
+		// In this case, a and n aren't coprime and we cannot calculate
+		// the inverse. This happens because the values of n are nearly
+		// prime (being the product of two primes) rather than truly
+		// prime.
+		return
+	}
+
+	if x.Cmp(bigOne) < 0 {
+		// 0 is not the multiplicative inverse of any element so, if x
+		// < 1, then x is negative.
+		x.Add(x, n)
+	}
+
+	return x, true
+}
+
+func digest(message string) []byte {
+	sha_256 := sha256.New()
+	sha_256.Write(bytes.NewBufferString(message).Bytes())
+	return sha_256.Sum(nil)
+}
+
+func encrypt(c *big.Int, pub *rsa.PublicKey, m *big.Int) *big.Int {
+	e := big.NewInt(int64(pub.E))
+	c.Exp(m, e, pub.N)
+	return c
 }
 
 func loadPublicKey(publicKeyStr string) (*rsa.PublicKey, error) {
