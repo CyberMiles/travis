57bd1168fc14ae6a00397321ca6a7b0e3a1040a5
dragon <longzhi@gmail.com>
2018-11-18 13:41:55 +0800

    fix a bug

--- modules/stake/types.go
+++ modules/stake/types.go
@@ -171,13 +171,11 @@ func (cs Candidates) Sort() {
 func (cs Candidates) updateVotingPower(blockHeight int64) Candidates {
 	// update voting power
 	for _, c := range cs {
+		c.PendingVotingPower = c.CalcVotingPower(blockHeight)
 		if c.Active == "N" {
 			c.VotingPower = 0
-		} else {
-			c.PendingVotingPower = c.CalcVotingPower(blockHeight)
-			if c.VotingPower != c.PendingVotingPower {
-				c.VotingPower = c.PendingVotingPower
-			}
+		} else if c.VotingPower != c.PendingVotingPower {
+			c.VotingPower = c.PendingVotingPower
 		}
 	}
 
